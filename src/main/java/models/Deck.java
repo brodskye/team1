a330a46f703d90781851cb2525cd283181087363
thompsoq <thompsoq@oregonstate.edu>
2017-11-27 17:14:01 -0800

    Update Deck.java

--- src/main/java/models/Deck.java
+++ src/main/java/models/Deck.java
@@ -12,12 +12,23 @@ public class Deck
 
     }
 
-    public void buildDeck() {
-        for(int i = 2; i < 15; i++){
-            deck.add(new Card(i,Suit.Clubs));
-            deck.add(new Card(i,Suit.Hearts));
-            deck.add(new Card(i,Suit.Diamonds));
-            deck.add(new Card(i,Suit.Spades));
+    public void buildDeck(int gameType) {
+        if (gameType == 0) {
+            for (int i = 2; i < 15; i++) {
+                deck.add(new Card(i, Suit.Clubs));
+                deck.add(new Card(i, Suit.Hearts));
+                deck.add(new Card(i, Suit.Diamonds));
+                deck.add(new Card(i, Suit.Spades));
+            }
+        } else {
+            for (int i = 2; i < 14; i++) {
+                deck.add(new Card(i, Suit.Bastos));
+                deck.add(new Card(i, Suit.Oros));
+                deck.add(new Card(i, Suit.Copas));
+                deck.add(new Card(i, Suit.Espadas));
+            }
+            deck.add(new Card(14, Suit.Wild));
+            deck.add(new Card(14, Suit.Wild));
         }
     }
 
